b054038e63893ef328b0caacdbc46b6a26e1e8b4
sangeetadammo09 <sangeetadammu12@gmail.com>
2024-10-22 10:53:06 +0530

    bootstrap link removed from index.html

--- src/app/common-layout/shared-components/header/header.component.ts
+++ src/app/common-layout/shared-components/header/header.component.ts
@@ -12,7 +12,34 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   shopSubmenuArr = ShopSubmenu;
-
+  menuList = [
+    {
+        id: 0,
+        name: 'Projects',
+        url:'/dashboard/project-list',
+        subMenu: [],
+        isActive: true
+    },
+    {
+        id: 1,
+        name: 'Metrics',
+        url:'',
+        subMenu: [
+            {
+                id: 0,
+                name: 'Application Development',
+                url:'/dashboard/application-development-metrics-list',
+            },
+            {
+                id: 1,
+                name: 'Application Support',
+                url:'/dashboard/application-support-metrics-list',
+            } 
+        ],
+        isActive: true
+    },
+    
+]; 
   ngOnInit(): void {
   }
 
